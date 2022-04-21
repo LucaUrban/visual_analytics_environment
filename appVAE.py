cb82415c5a33e4e244a35d5efd07a80e688187ae
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2022-04-21 08:37:28 +0200

    Update appVAE.py

--- appVAE.py
+++ appVAE.py
@@ -47,7 +47,7 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
         else:
             table = pd.read_csv(uploaded_file)
     else:
-        table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/visual_analytics_environment/main/eter dataset demo.csv')
+        table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/visual_analytics_environment/main/eter%20dataset%20demo.csv')
 
     # importing all other necessary files
     with urlopen('https://raw.githubusercontent.com/leakyMirror/map-of-europe/master/GeoJSON/europe.geojson') as response:
