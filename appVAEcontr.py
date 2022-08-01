747befd78fee890f0e8e6be89833712d467cc2fa
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2022-08-01 12:04:46 +0200

    Update appVAEcontr.py

--- appVAEcontr.py
+++ appVAEcontr.py
@@ -321,19 +321,19 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
         cou_sel = st.selectbox("Id to explore", ['All ids'] + list(table[ratio_vio_sel1].unique()), 0)
         if cou_sel == 'All ids':
             if ratio_vio_sel2 == '-':
-                fig_vio = px.violin(table, y = new_ratio_name, box = True, points = 'suspectedoutliers', title = 'Violin plot for the created ratio', hover_data = table[ratio_vio_sel1])
+                fig_vio = px.violin(table, y = new_ratio_name, box = True, points = 'suspectedoutliers', title = 'Violin plot for the created ratio', hover_data = ratio_vio_sel1)
             else:
                 table[ratio_vio_sel2].replace({np.nan: 'missing'}, inplace = True)
                 fig_vio = px.violin(table, y = new_ratio_name, color = table[ratio_vio_sel2], box = True, points = 'suspectedoutliers', 
-                                    title = 'Violin plot for the created ratio', hover_data = table[ratio_vio_sel1])
+                                    title = 'Violin plot for the created ratio', hover_data = ratio_vio_sel1)
         else:
             if ratio_vio_sel2 == '-':
                 fig_vio = px.violin(table[table[ratio_vio_sel1] == cou_sel], y = new_ratio_name, x = ratio_vio_sel1, box = True, points = 'suspectedoutliers', 
-                                    title = 'Violin plot for the created ratio', hover_data = table[ratio_vio_sel1])
+                                    title = 'Violin plot for the created ratio', hover_data = ratio_vio_sel1)
             else:
                 table[ratio_vio_sel2].replace({np.nan: 'missing'}, inplace = True)
                 fig_vio = px.violin(table[table[ratio_vio_sel1] == cou_sel], y = new_ratio_name, x = ratio_vio_sel1, color = table[table[ratio_vio_sel1] == cou_sel][ratio_vio_sel2], 
-                                    box = True, points = 'suspectedoutliers', title = 'Violin plot for the created ratio', hover_data = table[ratio_vio_sel1])
+                                    box = True, points = 'suspectedoutliers', title = 'Violin plot for the created ratio', hover_data = ratio_vio_sel1)
         st.plotly_chart(fig_vio, use_container_width=True)
 
         st.write('If you want to download the result file with the new ratio clik on the following button:')
