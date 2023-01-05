9ddb05a032deb047a979162e9696d75d049b2944
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-01-05 11:08:48 +0100

    Update appVAEcontr.py

--- appVAEcontr.py
+++ appVAEcontr.py
@@ -1104,6 +1104,6 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
                                                      on = con_checks_id_col)
                 df_cols = [con_checks_id_col] + descr_col + t_col + ['Variable', 'Trend', 'Detected case', 'Rupt. years']
             table_download['Variable'] = con_checks_features
-            #table_download = table_download[df_cols]
+            table_download = table_download[df_cols]
             st.download_button(label = "Download data with lables", data = table_download.to_csv(sep = ';').encode('utf-8'), file_name = 'result.csv', mime = 'text/csv')
            
