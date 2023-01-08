caef37f3e52ce04f7865a4e840b356f9355db941
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-01-08 12:50:13 +0100

    Update appVAEcontr.py

--- appVAEcontr.py
+++ appVAEcontr.py
@@ -934,8 +934,8 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
                                         for country in list_countries] for cat in list_un_cat])
                 DV_fin_res = np.append(DV_fin_res, np.sum(DV_fin_res, axis = 1).reshape((len(list_un_cat), 1)), axis = 1)
                 DV_fin_res = np.append(DV_fin_res, np.sum(DV_fin_res, axis = 0).reshape(1, len(list_countries)+1), axis = 0)
-                tab_abs = np.append(DV_fin_res, np.sum(tab_per, axis = 1).reshape((len(list_un_cat), 1)), axis = 1)
-                tab_abs = np.append(DV_fin_res, np.sum(tab_per, axis = 0).reshape(1, len(list_countries)+1), axis = 0)
+                tab_abs = np.append(tab_abs, np.sum(tab_per, axis = 1).reshape((len(list_un_cat), 1)), axis = 1)
+                tab_abs = np.append(tab_abs, np.sum(tab_per, axis = 0).reshape(1, len(list_countries)+1), axis = 0)
                 tab_per = 100*np.true_divide(DV_fin_res, tab_abs, out = np.zeros(DV_fin_res.shape, dtype=float), where = tab_abs!=0)
                 list_fin_res = [[f'{DV_fin_res[i, j]}\n({tab_per[i, j]})' for i in range(DV_fin_res.shape[0])] for j in range(DV_fin_res.shape[1])]
                 list_prob_cases = [[con_checks_feature, list_countries[j], list_un_cat[i], f'{round(tab_per[i, j], 2)}%', f'{DV_fin_res[i, j]}/{tab_abs[i, j]}'] if i != len(list_fin_res)-1 
