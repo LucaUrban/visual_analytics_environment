2140c25a5df31396c0e4c92c5cec455afa2035b3
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-01-08 10:09:11 +0100

    Update appVAEcontr.py

--- appVAEcontr.py
+++ appVAEcontr.py
@@ -904,7 +904,6 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
             if cat_sel_col == '-':
                 DV_fin_res = np.zeros((1, len(list_countries)), dtype = int)
                 for flag in ck_flags:
-                    st.write(flag)
                     DV_fin_res[0, list_countries.index(table[table[con_checks_id_col] == flag][country_sel_col].unique()[0])] += 1
             else:
                 list_un_cat = list(table[table[con_checks_id_col].isin(ck_flags)][cat_sel_col].unique())
