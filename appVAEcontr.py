10303675305cc505c4d85f08ff97fe4c5e0e75e4
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-01-13 10:55:21 +0100

    Update appVAEcontr.py

--- appVAEcontr.py
+++ appVAEcontr.py
@@ -879,7 +879,6 @@ if demo_data_radio == 'Demo datset' or uploaded_file is not None:
                     rupt_y += f'{not_na_years[i]}-'
                 
                 if rupt_y != '':
-                    st.write(table[table[con_checks_id_col] == id_inst].index)
                     table.loc[table[table[con_checks_id_col] == id_inst].index, 'Rupt. Years'] = rupt_y
             
             # computation of the geometric mean
